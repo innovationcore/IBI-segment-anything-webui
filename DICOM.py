3d0acf364b4a6ac79cb765626cd34cc43ecacd2c
Noah Perry <perrynoahchristian@gmail.com>
2024-07-12 09:39:04 -0400

    some stuff

---
+++ DICOM.py
@@ -0,0 +1,44 @@
+import os
+import glob
+import monai
+import torch
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+import SimpleITK as sitk
+from statistics import mean
+from torch.optim import Adam
+from natsort import natsorted
+import matplotlib.pyplot as plt
+from transformers import SamModel
+import matplotlib.patches as patches
+from transformers import SamProcessor
+from IPython.display import clear_output
+from torch.utils.data import Dataset, DataLoader
+from torch.nn.functional import threshold, normalize
+
+# not sure what this is, but it was in the tutorial
+# %matplotlib inline
+
+from monai.transforms import (
+    EnsureChannelFirstd,
+    EnsureTyped,
+    Compose,
+    CropForegroundd,
+    CopyItemsd,
+    LoadImaged,
+    CenterSpatialCropd,
+    Invertd,
+    OneOf,
+    Orientationd,
+    MapTransform,
+    NormalizeIntensityd,
+    RandSpatialCropSamplesd,
+    CenterSpatialCropd,
+    RandSpatialCropd,
+    SpatialPadd,
+    ScaleIntensityRanged,
+    Spacingd,
+    RepeatChanneld,
+    ToTensord,
+)
